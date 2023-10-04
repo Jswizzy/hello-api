d90e93906224f0966b35df3fed3216fe54541631
Justin Smith <smithjn@g.cofc.edu>
2023-10-03 22:45:45 -0400

    Add Testing

--- translation/translator_test.go
+++ translation/translator_test.go
@@ -6,15 +6,64 @@ import (
 )
 
 func TestTranslate(t *testing.T) {
-	// Arrange
-	word := "hello"
-	language := "english"
-
-	// Act
-	res := translation.Translate(word, language)
-
-	// Assert
-	if res != "hello" {
-		t.Errorf(`expected "hello" but received "%s"`, res)
+	tt := map[string]struct {
+		word     string
+		language string
+		want     string
+	}{
+		"English": {
+			word:     "hello",
+			language: "english",
+			want:     "hello",
+		},
+		"German": {
+			word:     "hello",
+			language: "german",
+			want:     "hallo",
+		},
+		"Finnish": {
+			word:     "hello",
+			language: "finnish",
+			want:     "hei",
+		},
+		"Dutch": {
+			word:     "hello",
+			language: "dutch",
+			want:     "",
+		},
+		"Missing": {
+			word:     "bye",
+			language: "dutch",
+			want:     "",
+		},
+		"Wrong": {
+			word:     "bye",
+			language: "german",
+			want:     "",
+		},
+		"Capital Word": {
+			word:     "Hello",
+			language: "german",
+			want:     "hallo",
+		},
+		"Capital Language": {
+			word:     "hello",
+			language: "German",
+			want:     "hallo",
+		},
+		"Space": {
+			word:     "hello ",
+			language: "german",
+			want:     "hallo",
+		},
 	}
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			res := translation.Translate(tc.word, tc.language)
+			if res != tc.want {
+				t.Errorf(`expected "%s" but recieved "%s"`, tc.want, res)
+			}
+		})
+	}
+
 }
